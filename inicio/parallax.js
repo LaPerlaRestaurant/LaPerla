a7afd0bf3ca45fa76a9479a3cfd37722aaea07e2
AgeX005 <agex005cm@gmail.com>
2024-05-02 01:49:21 -0600

    a

--- inicio/parallax.js
+++ inicio/parallax.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function(){
     var scrollPosition = window.scrollY;
-    document.querySelector('.section-1').style.setProperty('top', (scrollPosition*0.75) + 'px');
-    document.querySelector('.section-1 .content').style.setProperty('top', 50 - (scrollPosition*0.15) + "%");
+    document.querySelector('.parallax').style.setProperty('top', (scrollPosition*0.75) + 'px');
+    document.querySelector('.section-1 .content').style.setProperty('top', 50 - (scrollPosition*0.0) + "%");
 });
