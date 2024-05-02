3819d50d44c64b390481bb6f92b5051d97da1a6a
AgeX005 <76788560+AgeX005@users.noreply.github.com>
2024-05-01 20:08:37 -0600

    Delete scripts directory

--- scripts/parallax.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function(){
-    var scrollPosition = window.scrollY;
-    document.querySelector('.section-1').style.setProperty('top', (scrollPosition*0.75) + 'px');
-    document.querySelector('.section-1 .content').style.setProperty('top', 50 - (scrollPosition*0.15) + "%");
-});
