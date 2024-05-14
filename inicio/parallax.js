13c382aee943e6606c1753708d0d57fc2c0dec10
LaPerlaRestaurant <laperlarestaurante24@gmail.com>
2024-05-14 01:49:18 -0600

    a

--- inicio/parallax.js
+++ inicio/parallax.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', function(){
     var scrollPosition = window.scrollY;
-    document.querySelector('.parallax').style.setProperty('background-position-y', (scrollPosition*0.75) + 'px');
+    document.querySelector('.parallax').style.setProperty('background-position-y', 0 + (scrollPosition) + 'px');
 });
